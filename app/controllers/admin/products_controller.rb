ccb48ad2f2354995180b54f5ae3835e965ff2ffc
krismina la <krismina.la@gmail.com>
2022-05-05 05:20:14 +0000

    add http basic authenticate for env

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
-  http_basic_authenticate_with name: ENV["HTTP_BASIC_USER"], password: ENV["HTTP_BASIC_PASSWORD"]
+  # error when trying to authenticate through env. no error if user/pass is hardcoded
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
+
 
   def index
     @products = Product.order(id: :desc).all
