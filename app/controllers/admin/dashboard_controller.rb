7a0407ea1bb03f04690f5d7cd9d6778a026dd009
arismink <krismina.la@gmail.com>
2022-05-05 12:30:47 -0600

    add instance variable for product and category count. pass data to view

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,15 +1,12 @@
 class Admin::DashboardController < ApplicationController
 
+
   http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTH_USER"], password: ENV["HTTP_BASIC_AUTH_PASSWORD"]
 
+
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
-  def count_products
-    # display how many products are in database
-  end
-
-  def count_categories
-    # display how many categories are in database
-  end
 end
