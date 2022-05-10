9ea8339783e33d238901e69d9dd31dd55f0a48bf
arismink <krismina.la@gmail.com>
2022-05-10 15:28:22 -0600

    create tests for product model

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+
+    it 'creates a product with all fields defined' do
+
+    end
+
+    it 'validates name' do
+
+    end
+
+    it 'validates quantity' do
+    end
+
+    it 'validates price' do
+    end
+
+    it 'validates category' do
+
+    end
+
+  end
+end
