346e0427b2ef1519d23583449fb9701115b0d1b6
arismink <krismina.la@gmail.com>
2022-05-10 16:47:11 -0600

    add user spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it 'validates that password and password_confirm contain the same values.' do
+      @user = User.new(first_name: 'Sassy', last_name: 'Sally', email: 'test@a.ca', password: 'test', password_confirmation: 'qwerty' )
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'validates that password and password_confirm cannot be blank.' do
+      @user = User.new(first_name: 'Sassy', last_name: 'Sally', email: 'test@a.ca', password: '', password_confirmation: '' )
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+
+    end
+
+    it 'validates that email is unique' do
+      @user1 = User.new(first_name: 'Sassy', last_name: 'Sally', email: 'test@a.ca', password: 'test', password_confirmation: 'test' )
+      @user1.save
+
+      @user2 = User.new(first_name: 'Bumbling', last_name: 'Bob', email: 'test@a.ca', password: 'test', password_confirmation: 'test' )
+      @user2.save
+
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+
+    end
+
+    it 'validates that first_name, last_name and email are present' do
+      @user = User.new(first_name: nil, last_name: 'Sally', email: 'test@a.ca', password: 'test', password_confirmation: 'test' )
+
+      puts @user.errors.full_messages
+
+    end
+
+    it 'validates that password created has a minimum character length' do
+    
+    # password created must have minimum length
+    # you will need to add this to the model
+
+    end
+
+
+
+  end
+
+  describe '.authenticate_with_credentials' do
+
+  end
+
 end
