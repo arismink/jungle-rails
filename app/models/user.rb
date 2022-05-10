4cf20e29fdbafcbdf723e87113be3a58d8dfecc3
arismink <krismina.la@gmail.com>
2022-05-10 16:47:01 -0600

    add specs to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-
+  validates_uniqueness_of :email, on: :create
   # gives user model authentication methods via bcrypt
   has_secure_password
-  
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
