ac1c2eedf360d2724333b85e04d5a421effcb0c0
arismink <krismina.la@gmail.com>
2022-05-11 01:22:04 -0600

    add test that checks if product add to cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,11 +1,21 @@
-describe('Homepage testing', () => {
+describe('Add to Cart testing', () => {
   beforeEach(() => {
     cy.visit('/')
   });
   
   it("should visit root", () => {
 
-  }) 
+  });
+
+  it("can add a product to cart", () => {
+    cy.get(".products article").should("be.visible");
+    
+    // add item to cart
+    cy.get(".btn").first().click({force: true});
+    
+    // check if cart qty increased by 1 and contains item added
+
+  });
 
 
 
