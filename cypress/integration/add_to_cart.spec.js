50faa2a06e9b8c74416183b120b89024696e2228
arismink <krismina.la@gmail.com>
2022-05-11 16:15:52 -0600

    add spec to test if item was add to cart

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -11,9 +11,10 @@ describe('Add to Cart testing', () => {
     cy.get(".products article").should("be.visible");
     
     // add item to cart
-    cy.get(".btn").first().click({force: true});
+    cy.contains("Add").click({force: true});
     
     // check if cart qty increased by 1 and contains item added
+    cy.contains("My Cart").should("include.text", "1")
 
   });
 
