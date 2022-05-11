affa33c0d0a93343ee5fdea16933b99bef7c37d3
arismink <krismina.la@gmail.com>
2022-05-10 21:08:04 -0600

    add return nil test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -53,13 +53,23 @@ RSpec.describe User, type: :model do
   end
 
   describe '.authenticate_with_credentials' do
-    it 'calls the authenticate method and returns the correct instance' do
+    it 'calls the authenticate method and returns the user instance' do
       @user = User.new(first_name: 'Sassy', last_name: 'Sally', email: 'test@x.ca', password: 'test', password_confirmation: 'test' )
       @user.save
 
       result = User.authenticate_with_credentials('test@x.ca', 'test')
 
       expect(result).to be_a_kind_of User
+
+    end
+
+    it 'calls the authenticate method and returns nil' do
+      @user = User.new(first_name: 'Sassy', last_name: 'Sally', email: 'test@x.ca', password: 'test', password_confirmation: 'test' )
+      @user.save
+
+      result = User.authenticate_with_credentials('test@x.ca', 'blah')
+
+      expect(result).to be_nil
 
     end
 
